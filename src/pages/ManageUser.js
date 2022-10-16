8df5172fd769043e31ef564fc20bfb99c3eed257
Damar <damarsepti627@gmail.com>
2022-10-16 14:36:36 +0700

    manage user pages

--- src/pages/ManageUser.js
+++ src/pages/ManageUser.js
@@ -27,37 +27,42 @@ const ManageUser = () => {
   }, [navigate]);
 
   return (
-    <div className="container mx-auto mt-5 px-4">
+    <div className="container mx-auto my-5 px-4">
       <h1 className="font-bold text-4xl text-center">Manage User</h1>
-      <table className="table-auto mx-auto">
-        <thead>
-          <tr>
-            <th>No</th>
-            <th>Username</th>
-            <th>Name</th>
-            <th>Email</th>
-            <th>Role</th>
-            <th>Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>The Sliding Mr. Bones (Next Stop, Pottersville)</td>
-            <td>Malcolm Lockyer</td>
-            <td>1961</td>
-          </tr>
-          <tr>
-            <td>Witchy Woman</td>
-            <td>The Eagles</td>
-            <td>1972</td>
-          </tr>
-          <tr>
-            <td>Shining Star</td>
-            <td>Earth, Wind, and Fire</td>
-            <td>1975</td>
-          </tr>
-        </tbody>
-      </table>
+      <div className="overflow-x-auto relative mt-4">
+        <table className="table-auto mx-auto w-full text-sm text-left">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-100">
+            <tr>
+              <th className="py-4 px-6">No</th>
+              <th className="py-4 px-6">Username</th>
+              <th className="py-4 px-6">Name</th>
+              <th className="py-4 px-6">Email</th>
+              <th className="py-4 px-6">Role</th>
+              <th className="py-4 px-6">Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user, index) => (
+              <tr className="border-b" key={index}>
+                <td className="py-3 px-6">{index + 1}</td>
+                <td className="py-3 px-6">{user.username}</td>
+                <td className="py-3 px-6">{user.name}</td>
+                <td className="py-3 px-6">{user.email}</td>
+                <td className="py-3 px-6">{user.role}</td>
+                <td className="py-3 px-6">
+                  <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                    Edit
+                  </button>
+                  <button className="ml-2 bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+                    Hapus
+                  </button>
+                </td>
+
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
