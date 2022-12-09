ecacc05c544b7e21bb80799e4528473341ae5dc8
Damars <77232504+damarss@users.noreply.github.com>
2022-12-09 15:26:06 +0700

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import Cookies from "universal-cookie";
 
-const BASE_URL = "http://localhost:8080";
+const BASE_URL = "https://222011829.student.stis.ac.id/";
 const cookies = new Cookies();
 
 export default axios.create({
